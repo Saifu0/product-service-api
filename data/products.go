f7b16d8ce17bd23130532d9503f1829df17f810c
Saifur Rahman <saifurrahmankhankhan@gmail.com>
2021-07-31 14:30:29 +0530

    added POST and PUT handler

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -17,6 +18,11 @@ type Product struct {
 	Deletedon   string  `json:"-"`
 }
 
+func (p *Product) FromJSON(r io.Reader) error {
+	d := json.NewDecoder(r)
+	return d.Decode(p)
+}
+
 type Products []*Product
 
 func (p *Products) ToJSON(w io.Writer) error {
@@ -26,6 +32,41 @@ func (p *Products) ToJSON(w io.Writer) error {
 
 func GetProducts() Products {
 	return ProductList
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	ProductList = append(ProductList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	_, idx, err := findProduct(id)
+
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	ProductList[idx] = p
+
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for idx, p := range ProductList {
+		if p.ID == id {
+			return p, idx, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound
+}
+
+func getNextID() int {
+	lastId := ProductList[len(ProductList)-1].ID
+	return lastId + 1
 }
 
 var ProductList = []*Product{
--- handler/products.go
+++ handler/products.go
@@ -3,6 +3,8 @@ package handler
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/Saifu0/product-service-api/data"
 )
@@ -21,15 +23,86 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			p.l.Println("Invalid URI, more than one ID")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI, more than one capture group", g[0])
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		id, err := strconv.Atoi(g[0][1])
+		if err != nil {
+			p.l.Println("Unable to convert string ID into integer")
+			http.Error(rw, "Unable to convert into integer", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, rw, r)
+		p.l.Printf("ID: %d is got updated!", id)
+	}
+
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET request")
+
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 
 	if err != nil {
 		http.Error(rw, "Unable to encode json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST request")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT request")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to decode json", http.StatusBadRequest)
+		return
+	}
+
+	err = data.UpdateProduct(id, prod)
+
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+	}
+
+	if err != nil {
+		http.Error(rw, "Unable to update the product", http.StatusInternalServerError)
+		return
 	}
 }
